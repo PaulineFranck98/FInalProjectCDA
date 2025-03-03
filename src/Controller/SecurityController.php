07769ca5f734678d838222ca2cdcbf5a26d6e3a2
PaulineFranck98 <pauline-franck@hotmail.fr>
2025-03-03 16:40:42 +0100

    google authentification

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    // change this part
+    public const SCOPES = [
+        'google' => [],
+    ];
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -28,5 +35,25 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route("/connect/{service}", name:'connect_google', methods: ['GET'])]
+    public function connect(string $service, ClientRegistry $clientRegistry): RedirectResponse
+    {
+        if(! in_array($service, array_keys(self::SCOPES), true))
+        {
+            throw $this->createNotFoundException();
+        }
+
+
+        return $clientRegistry
+            ->getClient($service)
+            ->redirect(self::SCOPES[$service]);
+    }
+
+    #[Route('/connect/check/{service}', name:'connect_check_google', methods: ['GET', 'POST'])]
+    public function check()
+    {
+        return new Response(status: 200);
     }
 }
