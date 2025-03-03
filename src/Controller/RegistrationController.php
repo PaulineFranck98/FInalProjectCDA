a7d26d9ee589be1a967a88e0ad840948580186c6
PaulineFranck98 <pauline-franck@hotmail.fr>
2025-03-03 08:02:18 +0100

    messenger

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -49,7 +49,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('_preview_error');
+            return $this->redirectToRoute('/login');
         }
 
         return $this->render('registration/register.html.twig', [
